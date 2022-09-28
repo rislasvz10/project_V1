4b5757ed64af0f10fec40d34ff200b2852a78307
rislasvz <rislasvz.10@gmail.com>
2022-09-27 20:17:15 -0500

    update

--- src/controller/productsManager.js
+++ src/controller/productsManager.js
@@ -1,55 +1,19 @@
 const fs = require('fs');
 
-const pathFile = '../data/products.json';
+let pathFile = '../data/products.json'; 
 
-class ProductsManager{
+class ProductsManager {
     
-    findAllProducts = async () => {
-        if(fs.existsSync(pathFile)) {
-            let data = await fs.promises.readFile(pathFile, 'utf-8')
-            let products = JSON.parse(data)
-            return {status: "success", message: "Users"}
+    findAllProducts(){
+        if(fs.existsSync(pathFile)){
+            let data = fs.readFileSync(pathFile, 'utf-8')
+            let products = JSON.parse(data);
+            return { status: "success", message: products }
         } else {
-            return {status: "error", message: error.message}
+            return { status: "error", message: "Data No Existente" }
         }
     }
-
-    getproductsById = async (id) => {
-        if(fs.existsSync(pathFile)) {
-            let data = await fs.promises.readFile(pathFile, 'utf-8')
-            let products = JSON.parse(data)
-            let product = products.find( prod => prod.id === id)
-            if(product) return {status: "success", message: product}
-            return {status: "error", message: "Product not found"}
-        }
-        else {
-            return {status: "error", message: error.message}
-        }
-    }
-
-    SaveNewProduct = async (product) => {
-        if (!product.timestamp || !product.nombre || !product.descripcion 
-            || !product.codigo || !product.foto || !product.precio || !product.stock) 
-            return { status: "Missing Fields", message:"missing fields"}
-        try {
-            if(fs.existsSync(pathFile)){
-                let data = await fs.promises.readFile(pathFile, 'utf-8')
-                let products = JSON.parse(data)
-                let id = products[products.length-1].id+1
-                product.id = id
-                products.push(product)
-                await fs.promises.writeFile(pathFile, JSON.stringify(products, null, 2))
-                return {status: "success", message: "Product created"}
-            } else {
-                product.id = 1
-                await fs.promises.writeFile (pathFile, JSON.stringify([product], null, 2))
-                return {status: "success", message: "Product created"}
-            }
-        } catch (error) {
-            return {status: "error", message: error.message}
-        }
-    }
-
+    
 }
 
 
